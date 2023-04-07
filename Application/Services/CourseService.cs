500dc3b9116eaf395eb6ef544c51355ce9a8e419
Jake Lin <jake.lin@dash.com.au>
2023-04-07 21:18:32 +1000

    Add Course Controller and View

--- Application/Services/CourseService.cs
+++ Application/Services/CourseService.cs
@@ -1,11 +1,6 @@
 ﻿using CleanArch.Application.Interfaces;
 using CleanArch.Application.ViewModels;
 using CleanArch.Domain.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CleanArch.Application.Services
 {
--- Application/ViewModels/CourseViewModel.cs
+++ Application/ViewModels/CourseViewModel.cs
@@ -4,6 +4,6 @@ namespace CleanArch.Application.ViewModels
 {
     public class CourseViewModel
     {
-        public IEnumerable<Course>? Courses { get; set; }
+        public IEnumerable<Course> Courses { get; set; }
     }
 }
