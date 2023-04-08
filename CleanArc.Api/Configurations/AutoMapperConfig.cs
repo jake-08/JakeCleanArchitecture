f7d3048e7f86e00d3a836dec98228b5eb4aa9fc8
Jake Lin <jake.lin@dash.com.au>
2023-04-08 22:45:51 +1000

    Add AutoMapper Configuration to Api and MVC Projects

---
+++ CleanArc.Api/Configurations/AutoMapperConfig.cs
@@ -0,0 +1,13 @@
+﻿using CleanArch.Application.AutoMapper;
+
+namespace CleanArc.Api.Configurations
+{
+    public static class AutoMapperConfig
+    {
+        public static void RegisterAutoMapper(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(CleanArch.Application.AutoMapper.AutoMapperConfiguration));
+            AutoMapperConfiguration.RegisterMappings();
+        }
+    }
+}
