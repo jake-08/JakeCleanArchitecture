e9440c157cfad251df614d3ba4959ab138aec47c
Jake Lin <jake.lin@dash.com.au>
2023-04-08 21:08:55 +1000

    Add Domain.Core Project and Implement InMemoryBus

--- CleanArch.Infra.Bus/InMemoryBus.cs
+++ CleanArch.Infra.Bus/InMemoryBus.cs
@@ -1,6 +1,21 @@
-﻿namespace CleanArch.Infra.Bus
+﻿using CleanArch.Domain.Core.Bus;
+using CleanArch.Domain.Core.Commands;
+using MediatR;
+
+namespace CleanArch.Infra.Bus
 {
-    public class InMemoryBus
+    public sealed class InMemoryBus : IMediatorHandler
     {
+        private readonly IMediator _mediator;
+
+        public InMemoryBus(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public Task SendCommand<T>(T command) where T : Command
+        {
+            return _mediator.Send(command);
+        }
     }
 }
